5d15fe9e6a81ed10ee681de4627b681a0b1f51af
jayapatel1623 <120316165+jayapatel1623@users.noreply.github.com>
2023-03-19 00:52:48 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -2,7 +2,7 @@ import React from "react";
 import ListRender from "./ListRender";
 
 const Today = (props) => {
-  const date = new Date("3/19/2023");
+  const date = new Date("9/28/2022");
   
   const today=props.list.filter((itm)=>{return (itm.date===date.toLocaleDateString("en-US"))});
   
